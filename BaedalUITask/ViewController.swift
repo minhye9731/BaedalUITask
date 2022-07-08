197d031413908a3074b249c2c1957b81975de8ec
minhye9731 <jenney62639731@gmail.com>
2022-07-08 09:22:09 +0900

    Initial Commit

---
+++ BaedalUITask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaedalUITask
+//
+//  Created by 강민혜 on 7/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
