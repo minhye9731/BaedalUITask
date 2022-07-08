a7fde2f7c82a33881717763f082e8f3fae3c82bf
minhye9731 <jenney62639731@gmail.com>
2022-07-08 17:26:19 +0900

    배달의 민족 홈화면 UI 구성 (80%)

---
+++ BaedalUITask/GithubTest.swift
@@ -0,0 +1,12 @@
+//
+//  GithubTest.swift
+//  BaedalUITask
+//
+//  Created by 강민혜 on 7/8/22.
+//
+
+import Foundation
+
+struct test {
+    
+}
---
+++ BaedalUITask/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  BaedalUITask
+//
+//  Created by 강민혜 on 7/8/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    func setUI() {
+        
+        // 요소별 회색 테두리 설정
+        
+        // 텍스트필드 플레이스홀더
+        
+        // 텍스트필드 그림지 효과 설정
+    }
+
+ 
+
+}
