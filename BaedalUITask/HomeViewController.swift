6e8a193eb9e5e3bdaa3db7c648de7a40d857840d
minhye9731 <jenney62639731@gmail.com>
2022-07-09 23:57:05 +0900

    배달의 민족 홈 화면 UI 오토레이아웃 구성 - 텍스트필드 그림자 설정 - 상단뷰 하단 양측 모서리 둥글기 설정 - 메뉴 항목별 테두리 상세설정

--- BaedalUITask/HomeViewController.swift
+++ BaedalUITask/HomeViewController.swift
@@ -9,22 +9,32 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet var sectionImageViews: [UIImageView]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        setUI()
     }
-    
     
     func setUI() {
-        
         // 요소별 회색 테두리 설정
+        for i in sectionImageViews {
+            i.layer.borderWidth = 2
+            i.layer.borderColor = UIColor.systemGray5.cgColor
+            i.layer.cornerRadius = 10
+            i.clipsToBounds = true
+        }
         
-        // 텍스트필드 플레이스홀더
+        // 텍스트필드 그림자 효과 설정
+        searchButton.layer.shadowColor = UIColor.gray.cgColor
+        searchButton.layer.shadowOpacity = 0.5
+        searchButton.layer.shadowOffset = CGSize.zero
+        searchButton.layer.shadowRadius = 3
         
-        // 텍스트필드 그림지 효과 설정
+        // 상단뷰 하단의 2개 모서리만 둥글게 설정
+        topView.roundCorners(cornerRadius: 8, maskedCorners: [.layerMinXMaxYCorner, .layerMaxXMaxYCorner])
     }
-
- 
 
 }
---
+++ BaedalUITask/UIView.swift
@@ -0,0 +1,16 @@
+//
+//  UIView.swift
+//  BaedalUITask
+//
+//  Created by 강민혜 on 7/8/22.
+//
+
+import UIKit
+
+extension UIView {
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
+}
